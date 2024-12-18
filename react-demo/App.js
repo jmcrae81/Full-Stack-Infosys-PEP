f8c49b41f890f172ce13967230d89009c31fceeb
Jimmy McRae <jimmy.a.mcrae@gmail.com>
2024-12-17 22:16:28 -0500

    Create App.js

---
+++ react-demo/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './counterComponent/Counter';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <p>Press the buttons</p>
+        <Counter/>
+      </div>
+    </>
+  )
+}
+
+export default App;
